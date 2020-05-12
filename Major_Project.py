58415c4bd47fd652e5c7d80d0000f5b99372057b
R.Vaibhav <vaibhav.rao200@gmail.com>
2020-05-13 01:23:05 +0530

    Graphs on mean(plan_cpu) grouped by create_timestamp of batchTask
    Also changed names of image files.

--- Major_Project.py
+++ Major_Project.py
@@ -103,17 +103,17 @@ plt.title("plan_cpu vs real_cpu_max",fontsize=25)
 plt.show()
 
 
-# In[113]:
+# In[114]:
 
 
 plt.figure(figsize=(50,75))
 
-timeVsplanCpu_ofTask=batchTask.groupby(['create_timestamp'],as_index=False).sum()
+timeVsplanCpuSum_ofTask=batchTask.groupby(['create_timestamp'],as_index=False).sum()
 
 #removing negative values
-timeVsplanCpu_ofTask=timeVsplanCpu_ofTask[timeVsplanCpu_ofTask.create_timestamp>=0] 
-x=timeVsplanCpu_ofTask['create_timestamp']
-y=timeVsplanCpu_ofTask['plan_cpu']
+timeVsplanCpuSum_ofTask=timeVsplanCpuSum_ofTask[timeVsplanCpuSum_ofTask.create_timestamp>=0] 
+x=timeVsplanCpuSum_ofTask['create_timestamp']
+y=timeVsplanCpuSum_ofTask['plan_cpu']
 plt.subplot(311)
 plt.xlabel('Time in seconds',fontsize=50)
 plt.ylabel('Total Requested CPU',fontsize=50)
@@ -124,9 +124,9 @@ plt.title("Total requested CPU vs (Time>=0)",fontsize=60)
 plt.plot(x,y)
 
 #removing plan_cpu values>4000
-timeVsplanCpu_ofTask=timeVsplanCpu_ofTask[timeVsplanCpu_ofTask.plan_cpu<=4000]
-x=timeVsplanCpu_ofTask['create_timestamp']
-y=timeVsplanCpu_ofTask['plan_cpu']
+timeVsplanCpuSum_ofTask=timeVsplanCpuSum_ofTask[timeVsplanCpuSum_ofTask.plan_cpu<=4000]
+x=timeVsplanCpuSum_ofTask['create_timestamp']
+y=timeVsplanCpuSum_ofTask['plan_cpu']
 plt.subplot(312)
 plt.xlabel('Time in seconds',fontsize=50)
 plt.ylabel('Total Requested CPU',fontsize=50)
@@ -136,11 +136,12 @@ plt.xticks(fontsize=40)
 plt.title("(Total requested CPU<=4000) vs (Time>=0)",fontsize=60)
 plt.plot(x,y)
 
-#removing plan_cpu values==0
-timeVsplanCpu_ofTask=timeVsplanCpu_ofTask[timeVsplanCpu_ofTask.plan_cpu>0]
-# print(timeVsplanCpu_ofTask)
-x=timeVsplanCpu_ofTask['create_timestamp']
-y=timeVsplanCpu_ofTask['plan_cpu']
+#removing plan_cpu values==0 and >2500
+#removing time values >2000
+timeVsplanCpuSum_ofTask=timeVsplanCpuSum_ofTask[timeVsplanCpuSum_ofTask.plan_cpu>0]
+# print(timeVsplanCpuSum_ofTask)
+x=timeVsplanCpuSum_ofTask['create_timestamp']
+y=timeVsplanCpuSum_ofTask['plan_cpu']
 plt.subplot(313)
 plt.axis([0,2000,0,2500])
 plt.xlabel('Time in seconds',fontsize=50)
@@ -149,6 +150,57 @@ plt.ylabel('Total Requested CPU',fontsize=50)
 plt.yticks(fontsize=40)
 plt.xticks(fontsize=40)
 plt.title("Total requested CPU(>0 and <=2500) vs Time(0-2000)",fontsize=60)
+plt.plot(x,y)
+
+plt.show()
+
+
+# In[119]:
+
+
+timeVsplanCpuAvg_ofTask=batchTask.groupby(['create_timestamp'],as_index=False).mean()
+
+plt.figure(figsize=(50,75))
+#removing negative values
+timeVsplanCpuAvg_ofTask=timeVsplanCpuAvg_ofTask[timeVsplanCpuAvg_ofTask.create_timestamp>=0] 
+x=timeVsplanCpuAvg_ofTask['create_timestamp']
+y=timeVsplanCpuAvg_ofTask['plan_cpu']
+plt.subplot(311)
+plt.xlabel('Time in seconds',fontsize=50)
+plt.ylabel('Average Requested CPU',fontsize=50)
+# plt.xticks(index,x,fontsize=10,rotation=30)
+plt.yticks(fontsize=40)
+plt.xticks(fontsize=40)
+plt.title("Average requested CPU vs (Time>=0)",fontsize=60)
+plt.plot(x,y)
+
+#removing plan_cpu values>250
+timeVsplanCpuAvg_ofTask=timeVsplanCpuAvg_ofTask[timeVsplanCpuAvg_ofTask.plan_cpu<=250]
+x=timeVsplanCpuAvg_ofTask['create_timestamp']
+y=timeVsplanCpuAvg_ofTask['plan_cpu']
+plt.subplot(312)
+plt.xlabel('Time in seconds',fontsize=50)
+plt.ylabel('Average Requested CPU',fontsize=50)
+# plt.xticks(index,x,fontsize=10,rotation=30)
+plt.yticks(fontsize=40)
+plt.xticks(fontsize=40)
+plt.title("(Average requested CPU<=250) vs (Time>=0)",fontsize=60)
+plt.plot(x,y)
+
+#removing plan_cpu values==0 and >150
+#removing time values >2000
+timeVsplanCpuAvg_ofTask=timeVsplanCpuAvg_ofTask[timeVsplanCpuAvg_ofTask.plan_cpu>0]
+# print(timeVsplanCpuAvg_ofTask)
+x=timeVsplanCpuAvg_ofTask['create_timestamp']
+y=timeVsplanCpuAvg_ofTask['plan_cpu']
+plt.subplot(313)
+plt.axis([0,2000,0,150])
+plt.xlabel('Time in seconds',fontsize=50)
+plt.ylabel('Average Requested CPU',fontsize=50)
+# plt.xticks(index,x,fontsize=10,rotation=30)
+plt.yticks(fontsize=40)
+plt.xticks(fontsize=40)
+plt.title("Average requested CPU(>0 and <=150) vs Time(0-2000)",fontsize=60)
 plt.plot(x,y)
 
 plt.show()
